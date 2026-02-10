aba11a258ce05195b206015a11b3e3a4ea9a14fb
spectr-e <devs.josia@gmail.com>
2026-02-10 22:13:20 +0300

    learn C# - PetProject: Build and test a loop for entering new pet data

--- DotNetCSharpProjB/GuidedProject/Starter/obj/Debug/net10.0/Starter.AssemblyInfo.cs
+++ DotNetCSharpProjB/GuidedProject/Starter/obj/Debug/net10.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09b0a4c7a9c89c1a5c7a1af7256cc4d45a22b58f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
