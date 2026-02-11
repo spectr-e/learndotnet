afe52a029c9bb8db2758247af69054eb58ec1697
spectr-e <devs.josia@gmail.com>
2026-02-11 22:11:56 +0300

    learn C# - TypeCast&Conversions:  You can observe from the output that casting a decimal into a float is a narrowing conversion because you're losing precision.

--- DotNetCastingConv/Program.cs
+++ DotNetCastingConv/Program.cs
@@ -9,8 +9,14 @@
 // decimal myDecimal = myInt;
 // Console.WriteLine($"decimal: {myDecimal}");
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
