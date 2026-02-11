00cc6e64c4a1ab3bd12c8f644c881cf826658f3c
spectr-e <devs.josia@gmail.com>
2026-02-11 22:17:10 +0300

    learn C# - TypeCast&Conversions:  Use ToString() to convert a number to a string

--- DotNetCastingConv/Program.cs
+++ DotNetCastingConv/Program.cs
@@ -28,7 +28,14 @@
 // Console.WriteLine(message);
 
 // Convert a string to an int using the Parse() helper method
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// Convert a string to a int using the Convert class
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+
