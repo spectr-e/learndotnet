86b5257d6fb023b9e514ed1fd522cfaf6d765bac
spectr-e <devs.josia@gmail.com>
2026-02-11 22:10:01 +0300

    learn C# - TypeCast&Conversions:  widening conversion means that you're attempting to convert a value from a data type that could hold less information to a data type that can hold more information

--- DotNetCastingConv/Program.cs
+++ DotNetCastingConv/Program.cs
@@ -1,5 +1,10 @@
-﻿int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
