ba96b71be6aa5707f3c1086e4259a043d1ed08f5
spectr-e <devs.josia@gmail.com>
2026-02-11 22:15:31 +0300

    learn C# - TypeCast&Conversions:  Use ToString() to convert a number to a string

--- DotNetCastingConv/Program.cs
+++ DotNetCastingConv/Program.cs
@@ -15,8 +15,15 @@
 // int myInt = (int)myDecimal;
 // Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// Use ToString() to convert a number to a string
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
--- DotNetCastingConv/obj/Debug/net10.0/DotNetCastingConv.AssemblyInfo.cs
+++ DotNetCastingConv/obj/Debug/net10.0/DotNetCastingConv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCastingConv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa41f030e6e7493a4966349cae6034d4da3fff9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c16185c77a31be1a348f8c32414b7398bb61e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCastingConv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCastingConv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
