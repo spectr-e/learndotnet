ee64fed07767b01ce449974b71428f996beecd69
spectr-e <devs.josia@gmail.com>
2026-02-11 22:11:16 +0300

    learn C# - TypeCast&Conversions:  By adding the casting instruction (int), you're telling the C# compiler that you understand it's possible you'll lose that precision, and in this situation, it's fine.

--- DotNetCastingConv/Program.cs
+++ DotNetCastingConv/Program.cs
@@ -3,8 +3,14 @@
 // string result = first + second;
 // Console.WriteLine(result);
 
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = myInt;
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+decimal myDecimal = 3.14m;
 Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
