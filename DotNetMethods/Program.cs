330e1ff4cc8782e60cecf110e74a0cb4d77e1078
spectr-e <devs.josia@gmail.com>
2026-02-13 00:24:35 +0300

    learn C# - Methods: Build code with methods

--- DotNetMethods/Program.cs
+++ DotNetMethods/Program.cs
@@ -1,110 +1,20 @@
-﻿// int[] times = { 800, 1200, 1600, 2000 };
-// int diff = 0;
+﻿Random random = new Random();
 
-// Console.WriteLine("Enter current GMT");
-// int currentGMT = Convert.ToInt32(Console.ReadLine());
+int luck = random.Next(100);
 
-// Console.WriteLine("Current Medicine Schedule:");
+string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
 
-// /* Format and display medicine times */
-// DisplayTimes();
+string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
 
+string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
 
-// Console.WriteLine("Enter new GMT");
-// int newGMT = Convert.ToInt32(Console.ReadLine());
+string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
 
-// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-//     Console.WriteLine("Invalid GMT");
-// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-// {
-//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-//     AdjustTimes();
-// }
-// else
-// {
-//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-//     AdjustTimes();
-// }
+Console.WriteLine("A fortune teller whispers the following words:");
 
-// Console.WriteLine("New Medicine Schedule:");
+string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
 
-// /* Format and display medicine times */
-// DisplayTimes();
-
-// void DisplayTimes()
-// {
-//     /* Format and display medicine times */
-//     foreach (int val in times)
-//     {
-//         string time = val.ToString();
-//         int len = time.Length;
-
-//         if (len >= 3)
-//             time = time.Insert(len - 2, ":");
-//         else if (len == 2)
-//             time = time.Insert(0, "0:");
-//         else
-//             time = time.Insert(0, "0:0");
-
-//         Console.Write($"{time} ");
-//     }
-//     Console.WriteLine();
-// }
-
-// void AdjustTimes()
-// {
-//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
-//     for (int i = 0; i < times.Length; i++)
-//         times[i] = times[i] + diff % 2400;
-// }
-
-/*
-if ipAddress consists of 4 numbers
-and
-if each ipAddress number has no leading zeroes
-and
-if each ipAddress number is in range 0 - 255
-then ipAddress is valid
-else ipAddress is invalid
-*/
-
-string[] ipCollection = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
-
-foreach (var ipv4Input in ipCollection)
+for (int i = 0; i < 4; i++)
 {
-    if (ValidateLength(ipv4Input) && ValidateZeroes(ipv4Input) && ValidateRange(ipv4Input))
-        Console.WriteLine($"{ipv4Input} is a valid IPv4 address");
-    else
-        Console.WriteLine($"{ipv4Input} is an invalid IPv4 address");
-}
-
-bool ValidateLength(string ip)
-{
-    if (ip.Split(".", StringSplitOptions.RemoveEmptyEntries).Length == 4)
-        return true;
-    else
-        return false;
-}
-
-bool ValidateZeroes(string ip)
-{
-    foreach (var item in ip.Split(".", StringSplitOptions.RemoveEmptyEntries))
-    {
-        if (item.StartsWith("0"))
-            return false;
-    }
-    return true;
-}
-
-bool ValidateRange(string ip)
-{
-    int[] range = Array.ConvertAll(ip.Split(".", StringSplitOptions.RemoveEmptyEntries), int.Parse);
-
-    foreach (var item in range)
-    {
-        if (item <= 0 || item > 255)
-            return false;
-    }
-
-    return true;
+    Console.Write($"{text[i]} {fortune[i]} ");
 }
