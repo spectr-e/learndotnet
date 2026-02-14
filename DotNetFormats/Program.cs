c5490a00f5332fa54805726311c034e3b9a8c8d1
spectr-e <devs.josia@gmail.com>
2026-02-14 18:37:02 +0300

    learn C# - formats

---
+++ DotNetFormats/Program.cs
@@ -0,0 +1,26 @@
+﻿string first = "Hello";
+string second = "World";
+//composite formatting
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+//template literals
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+//formatting currency
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+// formatting numbers
+Console.WriteLine($"Measurement: {measurement:N} units");
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+// formatting percentages
+Console.WriteLine($"Tax rate: {tax:P2}");
