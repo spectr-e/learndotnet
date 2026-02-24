9407c7b9a93141ada7aeccfa235b4fd8b875e291
spectr-e <devs.josia@gmail.com>
2026-02-24 10:29:49 +0300

    learn blazor: BlazingPizzas - Handle the state of an order

--- DotNetBlazingPizza/Program.cs
+++ DotNetBlazingPizza/Program.cs
@@ -1,4 +1,5 @@
 using BlazingPizza.Data; //This statement allows the app to use the new service.
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
