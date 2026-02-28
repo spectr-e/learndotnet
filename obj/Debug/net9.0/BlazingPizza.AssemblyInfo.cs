7b7b3346a8048726e792510f485c463845f8a552
spectr-e <devs.josia@gmail.com>
2026-02-28 12:57:21 +0300

    learn blazor: BlazingPizzas - Add an orders page

--- obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d44a39fefe21a0f63db006fa38433851d48d3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3be2ee9aa88de607e6c9ed0203a7e0bdac3549")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
