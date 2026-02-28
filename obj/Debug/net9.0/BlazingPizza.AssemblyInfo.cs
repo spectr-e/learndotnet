100a63ae294603a7c127556403a1218a123e3152
spectr-e <devs.josia@gmail.com>
2026-02-28 14:01:18 +0300

    learn blazor: BlazingPizzas - Update all pages to the new layout

--- obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7b3346a8048726e792510f485c463845f8a552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d11492a38cc1193e71fea70c599f93829912b74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
