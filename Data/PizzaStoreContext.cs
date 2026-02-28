1c3be2ee9aa88de607e6c9ed0203a7e0bdac3549
spectr-e <devs.josia@gmail.com>
2026-02-28 12:17:16 +0300

    learn blazor: BlazingPizzas - Add entity framework support for orders and pizzas

--- Data/PizzaStoreContext.cs
+++ Data/PizzaStoreContext.cs
@@ -10,4 +10,19 @@ public class PizzaStoreContext : DbContext
 
   }
   public DbSet<PizzaSpecial> Specials { get; set; }
+  public DbSet<Pizza> Pizzas { get; set; }
+  public DbSet<Topping> Toppings { get; set; }
+  public DbSet<Order> Orders { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    // configuring a many-to-many special -> topping relationship that is friendly for serialization
+    modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+
+    modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+
+    modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+  }
+
 }
