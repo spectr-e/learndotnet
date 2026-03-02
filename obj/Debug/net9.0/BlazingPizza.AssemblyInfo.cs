b3cb298cf16f656e3075aace07bddd6b0b6b3715
spectr-e <devs.josia@gmail.com>
2026-03-02 20:38:05 +0300

    learn blazor: BlazingPizzas - Refactor the checkout page (AddressEditor)

--- obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a369d284f9a344917b6e50f2ba0c07341df2946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af3c5f10e6eecef2d41aa64b53e2411eb0647cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
