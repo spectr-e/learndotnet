c2fc1f0245cbf94286c117cd34bc6900ee36c80f
spectr-e <devs.josia@gmail.com>
2026-03-03 10:14:04 +0300

    Update BlazingPizza assembly information version and cache

--- obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d022c68146be71552719b5574e060f4b5eea26b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd164eef4772fda3a84c171bc92268d7d7f42d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
