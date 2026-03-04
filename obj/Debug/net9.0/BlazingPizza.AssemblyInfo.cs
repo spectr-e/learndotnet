4a4e34507012639e97d44c15b7cd395578b827d0
spectr-e <devs.josia@gmail.com>
2026-03-04 22:48:05 +0300

    learn blazor: BlazingPizzas - Fix MainLayout

--- obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fc1f0245cbf94286c117cd34bc6900ee36c80f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834cb1179feef9a19a3fecc7fcd65f76830760df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
